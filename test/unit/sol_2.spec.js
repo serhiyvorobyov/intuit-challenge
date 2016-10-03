367fe5ac84f60df11f69127cbbfa4f071039c38e
Serhiy Vorobyov <serhiy.vorobyov@gmail.com>
2016-10-03 10:11:27 -0700

    completed barebone structure of test cases for each problem

---
+++ test/unit/sol_2.spec.js
@@ -0,0 +1,3 @@
+describe("Problem 2 Test Cases", function () {
+	it("should define valid test");
+});
---
+++ test/unit/sol_3.spec.js
@@ -0,0 +1,3 @@
+describe("Problem 3 Test Cases", function () {
+	it("should define valid test");
+});
