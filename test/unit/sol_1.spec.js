ccb8c7f6afd3a5bde426cf6e33cb9f640dbd2b1a
Serhiy Vorobyov <serhiy.vorobyov@gmail.com>
2016-10-03 20:57:29 -0700

    added test cases for simple JSON object and array

--- test/unit/sol_1.spec.js
+++ test/unit/sol_1.spec.js
@@ -2,8 +2,8 @@ var p1 = require('../../sol_1.js');
 
 var expect = require('chai').expect;
 
-describe("Problem 1 Test Cases", function (done) {
-	it("should return null for an empty file", function () {
+describe('Problem 1 Test Cases', function (done) {
+	it('should return null for an empty file', function () {
 		p1('data/empty.json')
 		.then( function (path) {
 			expect(path).to.equal(null);
@@ -12,7 +12,7 @@ describe("Problem 1 Test Cases", function (done) {
 		
 	});
 
-	it("should return null for an empty object", function (done) {
+	it('should return null for an empty object', function (done) {
 		p1('data/empty_obj.json')
 		.then( function (path) {
 			expect(path).to.equal(null);
@@ -21,7 +21,7 @@ describe("Problem 1 Test Cases", function (done) {
 		
 	});
 
-	it("should return null for an empty array", function (done) {
+	it('should return null for an empty array', function (done) {
 		p1('data/empty_arr.json')
 		.then( function (path) {
 			expect(path).to.equal(null);
@@ -29,12 +29,25 @@ describe("Problem 1 Test Cases", function (done) {
 		});
 	});
 
-	it("finds item in a basic object with no nesting");
-	it("finds item in a basic array");
-	it("finds item in an object that has an array");
-	it("finds item in an array of basic objects");
-	it("finds item in an object with single nested objects");
-	it("finds item in an object with double nested objects");
-	it("finds item in an array of objects with nested objects and arrays");
-	it("passes the given sample test case");
+	it('finds item in a basic object with no nesting', function (done) {
+		p1('data/simple_obj.json', 'serhiy')
+		.then( function (path) {
+			expect(path).to.equal('\\name');
+			done();
+		})
+	});
+
+	it('finds item in a basic array', function (done) {
+		p1('data/simple_arr.json', 'serhiy')
+		.then( function (path) {
+			expect(path).to.equal('\\1');
+			done();
+		});
+	});
+	it('finds item in an object that has an array');
+	it('finds item in an array of basic objects');
+	it('finds item in an object with single nested objects');
+	it('finds item in an object with double nested objects');
+	it('finds item in an array of objects with nested objects and arrays');
+	it('passes the given sample test case');
 });
