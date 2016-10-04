48de167be3c92a229ef477ebb1567d418c4a3abe
Serhiy Vorobyov <serhiy.vorobyov@gmail.com>
2016-10-03 21:02:09 -0700

    test cases passing

--- sol_1.js
+++ sol_1.js
@@ -1,4 +1,8 @@
 var jsonfile = require('jsonfile');
+var _ = require('lodash');
+
+var currPath;
+var itemFound;
 
 /*
  * Returns the full path for an element if it exists in the
@@ -12,11 +16,13 @@ var jsonfile = require('jsonfile');
  * @returns {string} - The path to itemName or null if it does not exist.
  */
 function getItemPath (fileName, itemName) {
+	currPath = ['\\'];
+	itemFound = false;
+
 	return getJSONData(fileName)
 		.then( function resolveGetJSONData (data) {
 			console.log('resolvedGetJSONData', data);
-			console.log(!!data);
-			if (!!data) {
+			if (_.isEmpty( data )) {
 				console.log('Rejecting due to empty data', data);
 				return Promise.reject();
 			}
@@ -64,11 +70,51 @@ function getJSONData (fileName) {
  *
  * @returns {string} - The path from root to where the item is, or null if not found.
  */
- function findItem (data, itemName) {
+function findItem (data, itemName) {
+ 	console.log('findItem');
  	return new Promise( function (resolve, reject) {
- 		console.log(data);
- 		resolve("Some path");
+ 		traverseJSON( data, itemName );
+ 		console.log(currPath, '< currPath');
+ 		
+ 		if (currPath.length === 1) {
+ 			reject(null);
+ 		} else {
+ 			resolve(currPath.join(''));
+ 		}
  	});
- }
+}
 
- module.exports = getItemPath;
+/*
+ * Traverses the JSON object and looks for itemName. Uses recursion for nested
+ * objects.
+ *
+ * @param {Object} data - The JSON data through which to iterate.
+ * @param {string} itemName - The name of the item we are looking for in data. 
+ */
+function traverseJSON (data, itemName) {
+ 	console.log("traverseJSON", data, itemName);
+ 	for (var i in data) {
+ 		console.log(i, data[i], typeof data[i], data[i] === itemName);
+ 		currPath.push(i);
+ 		console.log('do I get here?');
+
+ 		if (itemFound) {
+ 			console.log('Breaking out');
+ 			break;
+ 		} else if (!!data[i] && typeof data[i] === 'object') {
+ 			console.log('recursive call');
+ 			traverseJSON( data[i], itemName );
+ 		} else if (data[i] === itemName) {
+ 			console.log('item found!');
+ 			itemFound = true;
+ 		}
+
+ 		if (!itemFound) {
+ 			console.log('popping from path');
+ 			currPath.pop();
+ 		}
+
+ 	}
+}
+
+module.exports = getItemPath;
